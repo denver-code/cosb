48f51263e4c2c0c43aa26d74e03d3cd480f0e12b
Ihor Savenko <csigorek@gmail.com>
2024-03-03 15:10:38 +0000

    external styles

--- main.py
+++ main.py
@@ -2,6 +2,19 @@ import json
 from moviepy.editor import VideoFileClip, TextClip, CompositeVideoClip
 
 
+def parse_style(style_file):
+    with open(style_file, "r") as file:
+        style_data = json.load(file)
+
+    name_colors = style_data.get("nameColors", {})
+    text_colors = style_data.get("textColors", {})
+    styles = style_data.get("styles", {})
+    padding = style_data.get("padding", 0)
+    duration = style_data.get("duration", 3)
+
+    return name_colors, text_colors, styles, padding, duration
+
+
 # Function to parse time string to seconds
 def parse_time(time_str):
     if ":" in time_str:
@@ -20,34 +33,39 @@ def load_subtitles(subtitle_file):
     return subtitles
 
 
-def add_subtitles_to_video(video_file, subtitle_file, output_file, padding=100):
+def add_subtitles_to_video(video_file, subtitle_file, output_file, style_file):
     video_clip = VideoFileClip(video_file)
     subtitles = load_subtitles(subtitle_file)
     clips = []
+    name_colors, text_colors, styles, padding, duration = parse_style(style_file)
 
     for subtitle in subtitles:
         start_time = parse_time(subtitle["startTime"])
-        end_time = start_time + 3
+        end_time = start_time + duration
         name = subtitle["name"] + ": "
         text = f"{subtitle['text']}"
 
         # Create the name and text clips with respective styles
         name_clip = TextClip(
             name,
-            fontsize=24,
-            color="#86B9E1",  # Blue color
+            fontsize=styles.get("fontSize", 24),
+            color=name_colors.get(subtitle["type"], "#C5E3BA"),
+            stroke_color=styles.get("strokeColor", "transparent"),
+            stroke_width=styles.get("stroke", 0),
         ).set_duration(end_time - start_time)
 
         text_clip = TextClip(
             text,
-            fontsize=24,
-            color="white",
+            fontsize=styles.get("fontSize", 24),
+            color=text_colors.get("general", "#F7F7F9"),
+            stroke_color=styles.get("strokeColor", "transparent"),
+            stroke_width=styles.get("stroke", 0),
         ).set_duration(end_time - start_time)
 
         # Concatenate name and text into one line
         combined_text_clip = TextClip(
             txt=name + text,
-            fontsize=24,
+            fontsize=styles.get("fontSize", 24),
         ).set_duration(end_time - start_time)
 
         # Calculate position for centering
@@ -71,7 +89,8 @@ def add_subtitles_to_video(video_file, subtitle_file, output_file, padding=100):
 
 
 # Example usage
+style_file = "style.json"
 video_file = "vid.mp4"
 subtitle_file = "subs.json"
 output_file = "output.mp4"
-add_subtitles_to_video(video_file, subtitle_file, output_file)
+add_subtitles_to_video(video_file, subtitle_file, output_file, style_file)
